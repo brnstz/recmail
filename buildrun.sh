ffba94229e96dfad9a99c0526e589939ebcb915a
Brian Seitz <brnstz@gmail.com>
2012-03-31 21:39:47 -0400

    - default values

--- buildrun.sh
+++ buildrun.sh
@@ -1,1 +1,1 @@
-go build -o recmail src/*.go && ./recmail -config=testdata/config.json -data=testdata/data-small.csv -template=testdata/th-recemail.html
+go build -o recmail src/*.go && ./recmail -config=testdata/config.json -data=testdata/data.csv -template=testdata/recmail.html
